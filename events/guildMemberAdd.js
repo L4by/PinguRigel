ffc847925327f1d16a4627d4d570e69face0b836
L4by <laby@laby.me>
2020-11-29 02:06:14 +0900

    이벤트(guildMemberAdd): 오류 수정

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,10 +4,10 @@ module.exports = async (client, member) => {
   const channel = member.guild.channels.cache.get('719089452802244659');
   if (!channel) return;
   const welcomeEmbed = new Discord.MessageEmbed()
-      .setAuthor(`${Discord.escapeMarkdown(member.tag)}님, 환영합니다!`)
+      .setAuthor(`${Discord.escapeMarkdown(member.user.tag)}님, 환영합니다!`)
       .setColor('ORANGE')
       .addField('안녕하세요!', [
-        `안녕하세요, ${Discord.escapeMarkdown(member.tag)}님! 다시 한 번 환영합니다.`,
+        `안녕하세요, ${Discord.escapeMarkdown(member.user.tag)}님! 다시 한 번 환영합니다.`,
         `활동 전 <#719086019558899745>, <#719086600260419625> 채널을 꼭 읽어주세요.`,
         `**이곳은 스터디 카페입니다. 게임 등 공부와 매우 동떨어진 주제의 대화는 삼가해주세요!**`,
       ].join('\n'))
