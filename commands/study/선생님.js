a5a26ad6ac2e1ea01e037660bfe0edad2b08309f
L4by <laby@laby.me>
2020-11-30 11:41:39 +0900

    명령어(선생님): 선생님 목록 추가

---
+++ commands/study/선생님.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const teacherRoles = message.guild.roles.cache.filter((role) => role.name.endsWith('쌤'));
+  if (!args[0]) {
+    const embed = new Discord.MessageEmbed()
+        .setColor('#FFD700')
+        .setAuthor(`선생님 목록 (전체 ${teacherRoles.size}명)`)
+        .setDescription([
+          '온라인인 선생님은 **굵은 글씨**로 표시됩니다.',
+          '자리 비움, 다른 용무 중, 오프라인인 선생님은 각각 __밑줄__, *기울인 글씨*, ~~취소선~~으로 표시됩니다.',
+        ].join('\n'))
+        .setFooter('For BlueRigel\'s Study Cafe | with <3');
+    teacherRoles.map((role) => {
+      embed.addField(role.name, role.members.map((teacher) => {
+        const status = teacher.presence.status;
+        let display;
+        if (status === 'offline') display = `~~${teacher}~~`;
+        else if (status === 'idle') display = `__${teacher}__`;
+        else if (status === 'dnd') display = `*${teacher}*`;
+        else if (status === 'online') display = `**${teacher}**`;
+        else display = teacher.toString();
+        return display;
+      }).join('\n'), true);
+    });
+    await message.channel.send(embed);
+  }
+};
+
+exports.conf = {
+  aliases: [],
+  permLevel: 'User',
+};
+
+exports.help = {
+  name: '선생님',
+  category: 'Study',
+  description: '질문을 받을 수 있는 선생님의 목록을 열람합니다.',
+  usage: '선생님',
+};
