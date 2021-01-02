7882211c1d487581d038ebfcf7e1dd71ff61ab3f
L4by <laby@laby.me>
2021-01-02 20:16:54 +0900

    이벤트(guildMemberUpdate): Collection 문법 수정

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -3,7 +3,7 @@ const path = require('path');
 const roleMembers = path.join(__dirname, '../../data/members.json');
 
 module.exports = async (client, oldMember, newMember) => {
-  if (!oldMember.roles.equals(newMember.roles) && fs.existsSync(roleMembers)) {
+  if (!oldMember.roles.cache.equals(newMember.roles) && fs.existsSync(roleMembers)) {
     const memberFile = JSON.parse(fs.readFileSync(roleMembers).toString());
     memberFile[newMember.id] = newMember.roles.cache.map((role) => role.id);
     fs.writeFileSync(roleMembers, JSON.stringify(memberFile, null, 2));
