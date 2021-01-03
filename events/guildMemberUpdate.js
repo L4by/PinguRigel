94d3493cf20b0c54534c875716b5d87e69b034fb
L4by <laby@laby.me>
2021-01-03 13:56:23 +0900

    이벤트(guildMember): 봇 예외 처리

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const roleMembers = './data/members.json';
 
 module.exports = async (client, oldMember, newMember) => {
+  if (oldMember.user.bot || newMember.user.bot) return;
   if (!oldMember.roles.cache.equals(newMember.roles) && fs.existsSync(roleMembers)) {
     const memberFile = JSON.parse(fs.readFileSync(roleMembers).toString());
     memberFile[newMember.id] = newMember.roles.cache.map((role) => role.id);
